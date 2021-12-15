c482bf34232a2f37f64f9bfbb9d736b2e4f2744d
Sanjeet Mistry <sanjeetm@axioned.com>
2021-12-15 15:30:34 +0530

    Add AJAX request for Upcoming Movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,9 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener('DOMContentLoaded', function(event) {
+	const xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "assets/json/upcoming.json");
+	xhttp.send();
+	xhttp.onload = function() {
+			let response = JSON.parse(this.responseText);
+			console.log(response);
+	}
+});
